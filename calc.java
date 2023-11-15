d85df4c861dd3b12d9b1c1fe632a78335afe053a
nayeem <ashiknay33@gmail.com>
2023-11-15 23:46:53 +0530

    Updated

--- calc.java
+++ calc.java
@@ -6,7 +6,7 @@ class Main {
 		      char operator;
 		          Double number1, number2, result;
 			      Scanner input = new Scanner(System.in);
-			          System.out.println("Choose an operator: +, -, *, or /");
+			          System.out.println("Choose any operator: +, -, *, or /");
 				      operator = input.next().charAt(0);
 				          System.out.println("Enter first number");
 					      number1 = input.nextDouble();
